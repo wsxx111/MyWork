b70deb735e737b7b6f72cf7018e22ed6e64b9f46
dashen <893130389@qq.com>
2016-11-08 09:49:14 +0800

    11月八号
    新整体伤筋动骨

---
+++ Work/LoveEveryDay/设计模式/我的整理/DesignMethod/观察者模式/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignMethod.观察者模式
+{
+    public class Program : OpenDesign
+    {
+        public override void Open()
+        {
+            #region 观察者模式
+
+            TenXun tenXun = new TenXunGame("腾讯游戏", "建立了一个腾讯游戏");
+            //添加订阅者
+            tenXun.AddObserver(new Subscriber("学习硬件"));
+            tenXun.AddObserver(new Subscriber("汤姆"));
+            tenXun.Update();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("移除汤姆订阅者");
+            tenXun.RemoveObserver(new Subscriber("汤姆"));
+            tenXun.Update();
+
+            #endregion 观察者模式
+
+            #region 观察者模式使用委托
+
+            //TenXunWt tenXun = new TenXunGameWt("腾讯游戏", "建立了一个腾讯游戏");
+            ////添加订阅者
+            //SubscriberWt lh = new SubscriberWt("学习硬件");
+            //SubscriberWt tom = new SubscriberWt("汤姆");
+            //tenXun.AddObserver(new 观察者模式.NotifyEventHandler(lh.ReceiveAndPrint));
+            //tenXun.AddObserver(new 观察者模式.NotifyEventHandler(tom.ReceiveAndPrint));
+            //tenXun.Update();
+            //Console.WriteLine("-----------------------------------");
+            //Console.WriteLine("移除汤姆订阅者");
+            //tenXun.RemoveObserver(new 观察者模式.NotifyEventHandler(tom.ReceiveAndPrint));
+            //tenXun.Update();
+
+            #endregion 观察者模式使用委托
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    //订阅号抽象类
+    public abstract class TenXun
+    {
+        //保留订阅号列表
+        private List<IObserver> observers = new List<IObserver>();
+
+        public TenXun(string symbol, string info)
+        {
+            this.Symbol = symbol;
+            this.Info = info;
+        }
+
+        public string Info { get; set; }
+        public string Symbol { get; set; }
+
+        #region 新增对订阅号列表的维护操作
+
+        public void AddObserver(IObserver ob)
+        {
+            observers.Add(ob);
+        }
+
+        public void RemoveObserver(IObserver ob)
+        {
+            observers.Remove(ob);
+        }
+
+        #endregion 新增对订阅号列表的维护操作
+
+        public void Update()
+        {
+            //遍历订阅者列表进行通知
+            foreach (IObserver ob in observers)
+            {
+                if (ob != null)
+                {
+                    ob.ReceiveAndPrint(this);
+                }
+            }
+        }
+    }
+
+    //具体订阅号
+    public class TenXunGame : TenXun
+    {
+        public TenXunGame(string symbol, string info)
+            : base(symbol, info)
+        { }
+    }
+
+
+    //订阅者接口
+    public interface IObserver
+    {
+        void ReceiveAndPrint(TenXun tenxun);
+    }
+
+    //具体订阅类
+    public class Subscriber : IObserver
+    {
+        public Subscriber(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void ReceiveAndPrint(TenXun tenxun)
+        {
+            Console.WriteLine("通知：{1}的{0}信息是: {2}", Name, tenxun.Symbol, tenxun.Info);
+        }
+    }
+
+
+
+
+
+
+
+    //委托--------------------------------------------------------------------------------------------------------------------------------------------------
+    public delegate void NotifyEventHandler(object sender);
+
+    //腾讯
+    public class TenXunWt
+    {
+        //保留订阅号列表
+        public event NotifyEventHandler NotifyEvent;
+
+        public TenXunWt(string symbol, string info)
+        {
+            this.Symbol = symbol;
+            this.Info = info;
+        }
+
+        public string Info { get; set; }
+        public string Symbol { get; set; }
+
+        #region 新增对订阅号列表的维护操作
+
+        public void AddObserver(NotifyEventHandler ob)
+        {
+            NotifyEvent += ob;
+        }
+
+        public void RemoveObserver(NotifyEventHandler ob)
+        {
+            NotifyEvent -= ob;
+        }
+
+        #endregion 新增对订阅号列表的维护操作
+
+        public void Update()
+        {
+            if (NotifyEvent != null)
+            {
+                NotifyEvent(this);
+            }
+        }
+    }
+
+
+    //腾讯游戏
+    public class TenXunGameWt : TenXunWt
+    {
+        public TenXunGameWt(string symbol, string info)
+            : base(symbol, info)
+        {
+        }
+    }
+
+    //委托
+    public class SubscriberWt
+    {
+        public SubscriberWt(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void ReceiveAndPrint(Object ob)
+        {
+            TenXunWt tenxun = ob as TenXunWt;
+            if (tenxun != null)
+            {
+                Console.WriteLine("通知：{1}的{0}信息是: {2}", Name, tenxun.Symbol, tenxun.Info);
+            }
+        }
+    }
+
+
+
+}
---
+++ Work/LoveEveryDay/设计模式/我的整理/DesignMethod/适配器/中文版.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignMethod.适配器
+{
+    #region  解析
+    /*    
+     *  适配器模式将一个接口转换成客户希望的另外一个接口。它使得原来由于接口不兼容而不能在一起工作的那些类可以一起工作。
+     * 
+     * 共有两类适配器模式：
+     * 对象适配器模式
+     * -- 在这种适配器模式中，适配器容纳一个它包裹的类的实例。在这种情况下，适配器调用被包裹对象的物理实体。
+     * 类适配器模式
+     * -- 这种适配器模式下，适配器继承自已实现的类（一般多重继承）。
+     * 
+     * 适配器模式的三个特点：
+     * 1 适配器对象实现原有接口 
+     * 2 适配器对象组合一个实现新接口的对象（这个对象也可以不实现一个接口，只是一个单纯的对象） 
+     * 3 对适配器原有接口方法的调用被委托给新接口的实例的特定方法 
+     * 
+     * */
+
+    #endregion
+
+    //客户端调用
+    public class 测试类
+    {
+        public static void 运行()
+        {
+            中标接口 zh = new 中国插座();           
+            适配器 ad = new 适配器(zh);
+            德国宾馆 hotel = new 德国宾馆();
+            hotel.设为德标(ad);
+            hotel.充电();          
+
+            Console.ReadKey();
+        }
+    }
+
+    //----------------------------------------------------------------------------------------
+
+   // 德国接口的插座通过 适配器 变成中国接口的中国插座
+
+    public abstract class 德标接口
+    {
+        public abstract void 两孔德国插座();
+    }
+
+
+    public class 德国插座 : 德标接口
+    {
+        public override void 两孔德国插座()
+        {
+            Console.WriteLine("现在是德国两孔插座");
+        }
+    }
+
+
+    public class 德国宾馆
+    {
+        private 德标接口 symbol;
+
+        public 德国宾馆()
+        {
+        }
+
+        public 德国宾馆(德标接口 symbolIn)
+        {
+            this.symbol = symbolIn;
+        }
+
+        public void 设为德标(德标接口 symbolIn)
+        {
+            this.symbol = symbolIn;
+        }
+
+        public void 充电()
+        {
+            symbol.两孔德国插座();
+        }
+    }
+
+
+    public abstract class 中标接口
+    {
+        public abstract void 两孔中国插座();
+    }
+
+    public class 中国插座 : 中标接口
+    {
+        public override void 两孔中国插座()
+        {
+            Console.WriteLine("现在是中国两孔插座");
+        }
+    }
+
+    public class 适配器 : 德标接口
+    {
+        private 中标接口 gbsymbol;
+
+        public 适配器(中标接口 gbsymbolIn)
+        {
+            this.gbsymbol = gbsymbolIn;
+        }
+
+        public override void 两孔德国插座()
+        {
+            gbsymbol.两孔中国插座();
+        }
+
+    }
+
+}
