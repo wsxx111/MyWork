3d4f5bc975a80d21c959de565e21cde5789ee5ea
dashen <893130389@qq.com>
2017-03-10 19:56:45 +0800

    3月10日
    最新更新 大更新

---
+++ LoveEveryDay/LoveEveryDay/WebUI/Resource/Javascript/menulist.js
@@ -0,0 +1,330 @@
+﻿/// <reference path="../../Content/总结/数据库/No-SQL数据库/MongoDb/index.html" />
+/// <reference path="../../Content/总结/数据库/No-SQL数据库/MongoDb/index.html" />
+var menuList = [{
+    "title": "主页",
+    "iconClass": "glyphicon glyphicon-home",
+    "url": "WebUI/Content/MainPage.html"
+},
+{
+    "title": "总结",
+    "iconClass": "glyphicon glyphicon-leaf",
+    "url": "",
+    "subMenuList": [
+        {
+            "title": "前端",
+            "iconClass": "",
+            "url": "",
+            "subMenuList":
+                [
+                     {
+                         "title": "html",
+                         "iconClass": "",
+                         "url": "",
+                         "subMenuList": [
+                             {
+
+                                 "title": "html基础",
+                                 "iconClass": "",
+                                 "url": "/WebUI/Content/总结/前端/Html/Html基础列表.html"
+                             },
+                              {
+                                  "title": "Canvas",
+                                  "iconClass": "",
+                                  "url": "Canvas.html"
+                              }
+                         ]
+                     },
+                    {
+                        "title": "Css",
+                        "iconClass": "",
+                        "url": "",
+                        "subMenuList": [
+                            {
+                                "title": "Css基础",
+                                "iconClass": "",
+                                "url": "/WebUI/Content/总结/前端/Css/Css基础列表.html"
+                            },
+                            {
+                                "title": "Css动画",
+                                "iconClass": "",
+                                "url": "/WebUI/Content/总结/前端/Css/Css动画/页面核心切换/test.html"
+                            },
+
+                             {
+                                 "title": "Less",
+                                 "iconClass": "",
+                                 "url": "/WebUI/Content/总结/前端/Css/Less/less.html"
+                             }
+                        ]
+                    },
+                     {
+                         "title": "JavaScript",
+                         "iconClass": "",
+                         "url": "",
+                         "subMenuList": [
+                             {
+                                 "title": "Javascript基础",
+                                 "iconClass": "",
+                                 "url": "Javascript基础.html"
+                             },
+                              {
+                                  "title": "Javascript面向对象",
+                                  "iconClass": "",
+                                  "url": "Javascript面向对象.html"
+                              }
+                         ]
+                     },
+                     {
+                         "title": "jQuery",
+                         "iconClass": "",
+                         "url": "",
+                         "subMenuList": [
+                             {
+                                 "title": "jQuery基础",
+                                 "iconClass": "",
+                                 "url": "WebUI/Content/总结/前端/jQuery/jquery基础.html"
+                             },
+                             {
+                                 "title": "jQuery插件",
+                                 "iconClass": "",
+                                 "url": "WebUI/Content/总结/前端/jQuery/jQuery插件学习/jQuery插件.html"
+                             },
+                              {
+                                  "title": "jQuery框架",
+                                  "iconClass": "",
+                                  "url": "jQuery框架.html"
+                              }
+                         ]
+                     },
+                     {
+                         "title": "VueJs",
+                         "iconClass": "",
+                         "url": "WebUI/Content/总结/前端/VueJs/vuejs.html"
+                     },
+                ]
+        },
+        {
+            "title": "后端",
+            "iconClass": "",
+            "url": "",
+            "subMenuList":
+               [
+                   {
+                       "title": "C#",
+                       "iconClass": "",
+                       "url": "",
+                       "subMenuList": [
+                           {
+                               "title": "C#平时总结",
+                               "iconClass": "",
+                               "url": "",
+                               "subMenuList": [
+                                   {
+                                       "title": "C#正则",
+                                       "iconClass": "",
+                                       "url": "/WebUI/Content/总结/后端/CSharp/CSharp平时总结/CSharp正则表达式.html"
+                                   },
+                                   {
+                                       "title": "Lambda表达式",
+                                       "iconClass": "",
+                                       "url": "/WebUI/Content/总结/后端/CSharp/CSharp平时总结/Lambda表达式.html"
+                                   },
+                                    {
+                                        "title": "Net的Cache",
+                                        "iconClass": "",
+                                        "url": "/WebUI/Content/总结/后端/CSharp/CSharp平时总结/Net的Cache.html"
+                                    },
+                                    {
+                                        "title": "反射",
+                                        "iconClass": "",
+                                        "url": "/WebUI/Content/总结/后端/CSharp/CSharp平时总结/反射.html"
+                                    },
+                                    {
+                                        "title": "异步线程",
+                                        "iconClass": "",
+                                        "url": "/WebUI/Content/总结/后端/CSharp/CSharp平时总结/异步线程.html"
+                                    },
+                                    {
+                                        "title": "并发编程",
+                                        "iconClass": "",
+                                        "url": "/WebUI/Content/总结/后端/CSharp/CSharp平时总结/并发编程.html"
+                                    }
+                               ]
+                           },
+                            {
+                                "title": "C#类库",
+                                "iconClass": "",
+                                "url": "",
+                                "subMenuList": [
+                       {
+                           "title": "C#类库",
+                           "iconClass": "",
+                           "url": "/WebUI/Content/总结/后端/CSharp/CSharp类库/codeShow.html"
+                       }
+                                ]
+                            },
+
+                       ]
+                   },
+                    {
+                        "title": "Linq",
+                        "iconClass": "",
+                        "url": "",
+                        "subMenuList": [
+                            {
+                                "title": "Linq基础",
+                                "iconClass": "",
+                                "url": "/WebUI/Content/总结/后端/Linq/linq.html"
+                            }
+                        ]
+                    },
+                    {
+                        "title": "设计模式",
+                        "iconClass": "",
+                        "url": "",
+                        "subMenuList": [
+                            {
+                                "title": "设计模式解析",
+                                "iconClass": "",
+                                "url": "/WebUI/Content/总结/后端/设计模式/codeShow.html"
+                            }
+                        ]
+                    },
+                    {
+                        "title": "实体框架EF",
+                        "iconClass": "",
+                        "url": "",
+                        "subMenuList": [
+                            {
+                                "title": "EF基础",
+                                "iconClass": "",
+                                "url": "/WebUI/Content/总结/后端/实体框架EF/EF基础.html"
+                            }
+                        ]
+                    }
+               ]
+        },
+        {
+            "title": "数据库",
+            "iconClass": "",
+            "url": "",
+            "subMenuList":
+               [
+                   {
+                       "title": "SQL数据库",
+                       "iconClass": "",
+                       "url": "",
+                       "subMenuList": [
+                           {
+                               "title": "sql平时总结",
+                               "iconClass": "",
+                               "url": "WebUI/Content/总结/数据库/SQL数据库/Sql Server/sqlserver.html"
+                           },
+                       {
+                           "title": "mysql平时总结",
+                           "iconClass": "",
+                           "url": "mysql平时总结.html"
+                       }
+                       ]
+                   },
+                   {
+                       "title": "No-SQL数据库",
+                       "iconClass": "",
+                       "url": "",
+                       "subMenuList": [
+                           {
+                               "title": "Memcache",
+                               "iconClass": "",
+                               "url": "Memcache数据库.html"
+                           },
+                       {
+                           "title": "Redis",
+                           "iconClass": "",
+                           "url": "Redis数据库.html"
+                       },
+                        {
+                            "title": "MongoDB",
+                            "iconClass": "",
+                            "url": "WebUI/Content/总结/数据库/No-SQL数据库/MongoDB/index.html"
+                        }
+                       ]
+                   },
+               ]
+        }]
+},
+{
+    "title": "教程",
+    "iconClass": "glyphicon glyphicon-adjust",
+    "subMenuList": [
+        {
+            "title": "前端",
+            "iconClass": "",
+            "url": "/前端.html"
+        },
+        {
+            "title": "后端",
+            "iconClass": "",
+            "url": "/后端.html"
+        },
+        {
+            "title": "数据库",
+            "iconClass": "",
+            "url": "/数据库.html"
+        }]
+},
+{
+    "title": "作品",
+    "iconClass": "glyphicon glyphicon-hdd",
+    "subMenuList": [
+        {
+            "title": "生活杂作",
+            "iconClass": "",
+            "url": "/WebUI/Content/作品/生活杂作/list.html"
+        },
+        {
+            "title": "作品2",
+            "iconClass": "",
+            "url": "/作品2.html"
+        },
+        {
+            "title": "作品3",
+            "iconClass": "",
+            "url": "/作品3.html"
+        }]
+},
+{
+    "title": "杂记",
+    "iconClass": "glyphicon glyphicon-tag",
+    "subMenuList": [
+        {
+            "title": "公司",
+            "iconClass": "",
+            "url": "/公司.html"
+        },
+        {
+            "title": "日常",
+            "iconClass": "",
+            "url": "/日常.html"
+        }]
+}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
